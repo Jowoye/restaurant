7734611bab75f1fd60806e29201083a7ec24a9e8
Maria Celeste Fernandez Zuñiga <maryce15_@hotmail.com>
2017-11-25 21:56:11 -0600

    añadir registro a la base de datos

---
+++ Resources/php/registrarse.php
@@ -0,0 +1,19 @@
+<?php
+	$dbc = mysqli_connect('localhost:8080','root','','restaurant')
+	or die('Error conectado a MYSQL');
+	
+	$nombre = $_POST['nombre'];
+	$pass = $_POST['password'];
+    $correo = $_POST['correo'];
+	
+	
+	$query = "INSERT INTO registro(nombre,contrasenna,correo) VALUES ('$nombre', $pass ,'$correo')";
+	
+	mysqli_query($dbc, $query)
+	or die ('Error ejecutando query en la BD');
+	
+	echo 'Registro exitoso';
+	
+	mysqli_close($dbc);
+	
+?>
