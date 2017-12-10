5377b85182e8766ff696ca14c7577ae3bd743d44
Maria Celeste Fernandez Zuñiga <maryce15_@hotmail.com>
2017-12-10 13:46:28 -0600

    add send index page

--- Resources/php/login.php
+++ Resources/php/login.php
@@ -4,14 +4,14 @@
 	
 	$email = $_POST['email'];
 	$pass = $_POST['pass'];
-	
+    $link = "<script>window.open(\"https://restaurant-happy-chef.herokuapp.com/\")</script>";
 	
 	$query = "SELECT * FROM `registro` WHERE correo = '$email' and contrasenna= '$pass'";
 	
 	mysqli_query($dbc, $query)
 	or die ('Error ejecutando query en la BD');
 	
-	echo 'Login exitoso';
+	echo 'Login exitoso', $link;
 	
 	mysqli_close($dbc);
 	
--- Resources/php/registrarse.php
+++ Resources/php/registrarse.php
@@ -5,14 +5,15 @@
 	$nombre = $_POST['nombre'];
 	$pass = $_POST['password'];
     $correo = $_POST['correo'];
-	
+	$link = "<script>window.open(\"https://restaurant-happy-chef.herokuapp.com/Login.html\")</script>";
+
 	
 	$query = "INSERT INTO `registro`(`nombre`, `contrasenna`, `correo`) VALUES('$nombre', '$pass' ,'$correo')";
 	
 	mysqli_query($dbc, $query)
 	or die ('Error ejecutando query en la BD');
 	
-	echo 'Registro exitoso';
+	echo 'Registro exitoso', $link;
 	
 	mysqli_close($dbc);
 	
